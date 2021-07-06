d158c0769564ca24d293e533263f3ee518a6afba
LinKrik <linkrik97@gmail.com>
2021-07-06 15:05:29 +0400

    Программа для управления модулем 1 (СЕЛЕКТОР)

---
+++ Selector/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Selector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitEvents();
+            selectorControl = new SelectorControl();
+        }
+
+        #region Поля
+        SelectorControl selectorControl;
+        #endregion
+
+        private void InitEvents()
+        {
+            //ListView
+            lstvwChanal.SelectionChanged += LstvwChanal_SelectionChanged;
+
+            //Grid
+            mainGrid.MouseLeftButtonDown += MainGrid_MouseLeftButtonDown;
+
+            //Button
+            btnMinimize.Click += BtnMinimize_Click;
+            btnClose.Click += BtnClose_Click;
+
+            //TagleButton
+            tbComPortConnection.Checked += tbComPortConnection_Checked; 
+            tbComPortConnection.Unchecked += tbComPortConnection_Unchecked;
+
+            //ComboBox
+            cbComPorts.SelectionChanged += CbComPorts_SelectionChanged;
+            cbComPorts.DropDownOpened += CbComPorts_DropDownOpened;
+
+            //checkBox
+            checkBoxShdnPower.Click += CheckBoxShdnPower_Click;
+        }
+
+        private void CheckBoxShdnPower_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectorControl.IsConnected)
+            {
+                try
+                {
+                    selectorControl.ActivationSHDN((bool)checkBoxShdnPower.IsChecked);
+                }
+                catch (Exception)
+                {
+                    selectorControl.Disconnect();
+                    tbComPortConnection.IsChecked = false;
+                }
+
+            }
+        }
+
+        private void LstvwChanal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (selectorControl.IsConnected)
+            {
+                try
+                {
+                    selectorControl.ActivationChannels(lstvwChanal.SelectedIndex);
+                }
+                catch (Exception)
+                {
+                    selectorControl.Disconnect();
+                    tbComPortConnection.IsChecked = false;
+                }
+                
+            }
+        }
+
+        private void MainGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CbComPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbComPorts.SelectedItem != null)
+            {
+                selectorControl.NameComPort = Convert.ToString(cbComPorts.SelectedItem); //присваиваем новое имя COMPORT
+            }
+        }
+
+        private void CbComPorts_DropDownOpened(object sender, EventArgs e)
+        {
+            cbComPorts.Items.Clear();
+            string[] ports = selectorControl.SearchComPorts();
+            foreach (var item in ports)
+            {
+                cbComPorts.Items.Add(item);
+            }
+        }
+
+        private void tbComPortConnection_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                Cursor = Cursors.Wait;
+                bool isConnect = selectorControl.Connect();
+                if (isConnect)
+                {
+                    SetCheckIcon(statusConnection);
+                    cbComPorts.IsEnabled = false;
+                    //tbComPortConnection.Content = "Закрыть";
+                }
+                else
+                {
+                    SetCheckIcon(statusConnection);
+                    tbComPortConnection.IsChecked = false;
+                    cbComPorts.IsEnabled = true;
+                }
+                Cursor = default;
+            }
+            catch (Exception)
+            {
+                SetCanselIcon(statusConnection);
+                tbComPortConnection.IsChecked = false;
+                cbComPorts.IsEnabled = true;
+                Cursor = default;
+
+            }
+        }
+
+        private void tbComPortConnection_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                Cursor = Cursors.Wait;
+                SetCanselIcon(statusConnection);
+                cbComPorts.IsEnabled = true;
+                selectorControl.Disconnect();
+                //tbComPortConnection.Content = "Открыть";
+                Cursor = default;
+            }
+            catch (Exception)
+            {
+                SetCheckIcon(statusConnection);
+                cbComPorts.IsEnabled = false;
+                tbComPortConnection.IsChecked = true;
+                Cursor = default;
+            }
+        }
+
+
+        private void SetCanselIcon(GeometryGroup geometryG)
+        {
+            GeometryGroup geometryGroup = geometryG;
+            geometryGroup.Children.Remove((Geometry)Application.Current.Resources["check"]);
+            geometryGroup.Children.Add((Geometry)Application.Current.Resources["cancel"]);
+            pathStatusConnection.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0x2E, 0x00));
+        }
+
+        private void SetCheckIcon(GeometryGroup geometryG)
+        {
+            GeometryGroup geometryGroup = geometryG;
+            geometryGroup.Children.Remove((Geometry)Application.Current.Resources["cancel"]);
+            geometryGroup.Children.Add((Geometry)Application.Current.Resources["check"]);
+            pathStatusConnection.Fill = new SolidColorBrush(Color.FromArgb(0xFF, 0x00, 0xFF, 0x0C));
+        }
+
+
+
+    }
+}
---
+++ Selector/SelectorControl.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Management;
+using System.Text;
+
+namespace Selector
+{
+    
+
+    public class SelectorControl
+    {
+
+        private byte[] _chanals = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120 };
+        SerialPort _comPort;
+        private bool _isConnected;
+        private string _portName;
+
+        public SelectorControl()
+        {
+            _comPort = null;
+            _isConnected = false;
+        }
+
+        public bool IsConnected => _isConnected;
+        public string NameComPort
+        {
+            get { return _portName; }
+            set 
+            {
+                if (!IsConnected)
+                {
+                    _portName = value;
+                }
+            }
+        }
+
+
+        public string[] SearchComPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public bool Connect()
+        {
+            if (_comPort == null)
+            {
+                _comPort = new SerialPort(_portName, 115200, Parity.None, 8, StopBits.One);
+                _comPort.WriteTimeout = 3000;
+                _comPort.ReadTimeout = 3000;
+            }
+            else
+                Disconnect();
+            try
+            {
+                _comPort.PortName = _portName;
+                _comPort.Open();
+
+                _isConnected = TestConnection();
+            }
+            catch (Exception ex)
+            {
+                
+                _isConnected = false;
+            }
+            return _isConnected;
+        }
+
+
+        public void Disconnect()
+        {
+            if (_comPort != null)
+            {
+                _comPort.Close();
+                _isConnected = false;
+            }
+        }
+
+        
+
+        private bool TestConnection()
+        {
+            int byteReadNumber = 1;
+            byte[] bytesToWrite = {200};
+            byte[] bytesToRead = new byte[byteReadNumber];
+
+            try
+            {
+                _comPort.Write(bytesToWrite, 0, 1);
+                _comPort.Read(bytesToRead, 0, byteReadNumber);
+            }
+            catch (Exception)
+            {
+                return false; 
+            }
+            
+
+
+            if (!(bytesToWrite[0] == bytesToRead[0]))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ActivationSHDN(bool activate)
+        {
+            int byteReadNumber = 1;
+            byte[] bytesToWrite = { 5, 0 };
+            byte[] bytesToRead = new byte[byteReadNumber];
+
+
+            try
+            {
+                _comPort.Write(bytesToWrite, 0, 1);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Write error", ex);
+            }
+
+            try
+            {
+                _comPort.Read(bytesToRead, 0, 1);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Read error", ex);
+            }
+
+            if (activate)
+            {
+                bytesToWrite[1] = 1;
+                try
+                {
+                    _comPort.Write(bytesToWrite, 1, 1);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Write error", ex);
+                }
+                
+            }
+            else
+            {
+                try
+                {
+                    _comPort.Write(bytesToWrite, 1, 1);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Write error", ex);
+                }
+               
+            }
+        }
+
+        /// <summary>
+        /// Активация выбранного канала Селектора
+        /// </summary>
+        /// <param name="channal">номер канала от 0 до 11 (0 - это 1-ый канал...11 - это 12-ый канал)</param>
+        public void ActivationChannels(int channal)
+        {
+
+            int N = 1;
+            byte[] bytesToWrite = new byte[N];
+            byte[] bytesToRead = new byte[N];
+            bytesToWrite[0] = _chanals[channal];
+
+            try
+            {
+                _comPort.Write(bytesToWrite, 0, 1);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Write error", ex);
+            }
+            try
+            {
+                _comPort.Read(bytesToRead, 0, 1);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Read error", ex);
+            }
+            
+        }
+    }
+}
